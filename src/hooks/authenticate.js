cd5932ee80c9e5effcb852bc2a6f934360b1a92b
brettsodie <blsodie@gmail.com>
2020-12-30 20:57:12 -0500

    fix: update auth header

--- src/hooks/authenticate.js
+++ src/hooks/authenticate.js
@@ -5,39 +5,14 @@ import throwError from '../utilities/throwError.js';
 
 const authenticate = async req => {
   let rawToken = null;
-  let propertyToken = null;
-  let functionToken = null;
-
-  console.log('RAW', req.rawHeaders);
-  console.log('REGULAR', req.headers);
 
   for (let i = 0; i < req.rawHeaders.length; i++) {
-    if (req.rawHeaders[i] === 'authorization') {
+    if (req.rawHeaders[i].toLowerCase() === 'authorization') {
       rawToken = req.rawHeaders[i + 1];
     }
   }
 
-  try {
-    propertyToken = req.headers['authorization'];
-  } catch(e) {
-    console.log('PROPERTY ERROR', e);
-  }
-
-  try {
-    functionToken = req.header('authorization');
-  } catch(e) {
-    console.log('FUNCTION ERROR', e);
-  }
-
-  console.log(rawToken, propertyToken, functionToken);
-
-  // const token = req.headers && req.headers['authorization'] 
-  //   ? req.headers['authorization'] 
-  //   : typeof req.header === 'function' 
-  //     ? req.header('authorization') 
-  //     : null;
-
-  const finalToken = rawToken || propertyToken || functionToken;
+  const finalToken = rawToken;
 
   if (!finalToken) throwError(401, 'Unauthorized');
 
