a3ce61b97ed359a590918b04eee196ef616c5de4
brettsodie <blsodie@gmail.com>
2020-12-30 19:07:45 -0500

    fix: add console log

--- src/hooks/authenticate.js
+++ src/hooks/authenticate.js
@@ -4,6 +4,8 @@ import app from '../index.js';
 import throwError from '../utilities/throwError.js';
 
 const authenticate = async req => {
+  console.log(req.headers);
+  console.log(req.header);
   
   const token = req.headers && req.headers['authorization'] ? req.headers['authorization'] : req.header('authorization');
 
